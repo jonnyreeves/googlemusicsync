519f02aabccd014b687eaaafca35add46cf266b4
Jonny Reeves <github@jonnyreeves.co.uk>
2014-04-22 14:50:12 +0100

    Externalised configuration, cleaned up logging

--- GoogleMusicSync.py
+++ GoogleMusicSync.py
@@ -8,9 +8,8 @@ from operator import itemgetter
 from appdirs import AppDirs
 from gmusicapi import Musicmanager
 
-MUSIC_LIBRARY="/Users/jonny/Desktop/foo"
 APP_DIRS = AppDirs('GoogleMusicSync', 'Jonny Reeves')
-RECENTLY_ADDED_SIZE = 100
+DEFAULT_SYNC_COUNT = 200
 
 def create_path_if_not_exist(path):
     try:
@@ -19,24 +18,37 @@ def create_path_if_not_exist(path):
         if exception.errno != errno.EEXIST:
             raise
 
-def download_track(mm, track):
-	print "Downloading {0}/{1}/{2} to {3}".format(track['artist'], track['album'], track['title'], MUSIC_LIBRARY)
+def download_track(mm, track, local_library):
+	artist = track['artist'].encode('utf-8')
+	album = track['album'].encode('utf-8')
+	title = track['title'].encode('utf-8')
+
+	print "Requesting {0} - {1} - {2}".format(artist, album, title)
 
 	filename, audio = mm.download_song(track["id"])
 	filename =  filename.encode('utf-8')
-	
-	target_dir = os.path.join(MUSIC_LIBRARY, track["artist"], track["album"])
+
+	target_dir = os.path.join(local_library, artist, album)
 	target_path = os.path.join(target_dir, filename)
+
 	create_path_if_not_exist(target_dir)
 
 	print "Writing " + target_path
 	with open(target_path, 'wb') as f:
 		f.write(audio)
 
+# Read configuration
+try:
+	with open('settings.json') as config_file:
+		cfg = json.load(config_file)
+		assert os.path.isdir(cfg['library'])
+except:
+	print "Failed to load configuration from settings.json"
+	sys.exit();
 
 # Login to GoogleMusic's Mobile API to get library metada
 api = Mobileclient()
-api.login('john@jonnyreeves.co.uk', 'hcctlielengpllcp')
+api.login(cfg["email"], cfg["password"])
 
 # And now login to the Musicmanager to perform downloads.
 mm = Musicmanager()
@@ -52,7 +64,7 @@ print "Reading local Track Ids from: " + track_ids_file
 try:
 	with open (track_ids_file, "r") as file_handle:
 		local_track_ids = json.loads(file_handle.read())
-		print "Loaded " + str(len(local_track_ids)) + " Track Id(s) from " + track_ids_file
+		print "Loaded {0} Track Id(s)".format(len(local_track_ids))
 except IOError as io_error:
 	if io_error.errno == errno.ENOENT:
 		local_track_ids = []
@@ -62,14 +74,22 @@ except IOError as io_error:
 
 # Fetch the user's library from GoogleMusic and then flter by created timestamp to get
 # a list of recently added tracks
-print "Fetching the last {0} tracks from Google Music".format(RECENTLY_ADDED_SIZE)
-last_added = sorted(api.get_all_songs(), key=itemgetter('creationTimestamp'), reverse=True)[:RECENTLY_ADDED_SIZE]
+print "Fetching library from Google Music"
+library = api.get_all_songs()
+
+sync_count = cfg['sync_count'] or DEFAULT_SYNC_COUNT
+print "Filtering {0} most recently added tracks".format(cfg['sync_count'])
+last_added = sorted(library, key=itemgetter('creationTimestamp'), reverse=True)[:sync_count]
+
 tracks_by_id = {}
+remote_track_ids = []
 
+# Keep tracks in order to make the downloads appear in the expected order.
 for track in last_added:
-	tracks_by_id[track['id']] = track
+	track_id = track['id']
+	tracks_by_id[track_id] = track
+	remote_track_ids.append(track_id)
 
-remote_track_ids = list(tracks_by_id.keys())
 if (not remote_track_ids):
 	print "No tracks fetched from Google Music API - exiting"
 	sys.exit();
@@ -82,21 +102,13 @@ remote_set = set(remote_track_ids)
 new_track_ids = list(remote_set.difference(local_set));
 
 if ((local_track_ids) and (new_track_ids)):
-	print "Found " + str(len(new_track_ids)) + " new Track Id(s) on Google Music"
-
-	# Download each track
+	print "Found {0} new Track Id(s) on Google Music since last sync".format(len(new_track_ids))
 	for track_id in new_track_ids:
-		download_track(mm, tracks_by_id[track_id])
-
+		download_track(mm, tracks_by_id[track_id], cfg['library'])
 else:
 	print "No new tracks found."
 
 if ((not local_track_ids) or (new_track_ids)):
-	# Save the updated local track list.
-	print "Writing " + str(len(new_track_ids)) + " new Track Id(s) to: " + track_ids_file
+	print "Writing {0} new Track Id(s) to {1}".format(len(new_track_ids), track_ids_file)
 	with open(track_ids_file, 'w') as file_handle:
 		file_handle.write(json.dumps(remote_track_ids))
-
-
-
-
